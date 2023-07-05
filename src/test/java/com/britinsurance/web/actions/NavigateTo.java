69237dad6f3a79aad4dd84597c3354a0298f5ffc
iminder.rehal <Iminder.Rehal@bottomline.com>
2023-07-05 15:29:42 +0100

    feat: small improvements

--- src/test/java/com/britinsurance/web/actions/NavigateTo.java
+++ src/test/java/com/britinsurance/web/actions/NavigateTo.java
@@ -6,6 +6,7 @@ import com.britinsurance.web.components.Menu;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.MoveMouse;
 import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actions.Scroll;
 import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
@@ -22,8 +23,8 @@ public class NavigateTo {
   public static Performable contactPageFromTheMenu() {
     return Task.where("{0} navigates to the Contact page from the menu",
         Wait.until(WebElementQuestion.the(Menu.MENU_BUTTON), WebElementStateMatchers.isClickable()),
-        Click.on(Menu.MENU_BUTTON)
-             .afterWaitingUntilEnabled(),
+        MoveMouse.to(Menu.MENU_BUTTON),
+        Click.on(Menu.MENU_BUTTON),
         Scroll.to(Menu.CONTACT_LINK)
               .then(Click.on(Menu.CONTACT_LINK))
     );
--- src/test/java/com/britinsurance/web/actions/Search.java
+++ src/test/java/com/britinsurance/web/actions/Search.java
@@ -6,6 +6,7 @@ import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.MoveMouse;
 import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
 import net.serenitybdd.screenplay.questions.WebElementQuestion;
 import net.serenitybdd.screenplay.waits.Wait;
@@ -21,11 +22,13 @@ public class Search {
   private final static int WAIT_TIME = 90;
   public static Performable byTerm(String keyword) {
     return Task.where("{0} searches for " + keyword + " in the top right search bar",
-        Wait.until(WebElementQuestion.the(SEARCH_BUTTON), WebElementStateMatchers.isPresent())
+        Wait.until(WebElementQuestion.the(SEARCH_BUTTON), WebElementStateMatchers.isClickable())
             .forNoMoreThan(Duration.ofSeconds(WAIT_TIME)),
+        MoveMouse.to(SEARCH_BUTTON),
         Click.on(SEARCH_BUTTON),
         Wait.until(WebElementQuestion.the(SEARCH_INPUT), WebElementStateMatchers.isPresent())
             .forNoMoreThan(Duration.ofSeconds(WAIT_TIME)),
+        MoveMouse.to(SEARCH_INPUT),
         Click.on(SEARCH_INPUT),
         Enter.theValue(keyword).into(SEARCH_INPUT).thenHit(Keys.ENTER),
         Wait.until(WebElementQuestion.the(SearchResults.RESULTS_HEADER), WebElementStateMatchers.isPresent())

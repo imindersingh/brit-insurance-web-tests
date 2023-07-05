7734031003360f494e53e8ed4a3c638194e2fc38
iminder.rehal <Iminder.Rehal@bottomline.com>
2023-07-05 13:24:28 +0100

    feat: updating selector to be more specific

--- src/test/java/com/britinsurance/web/components/SearchBar.java
+++ src/test/java/com/britinsurance/web/components/SearchBar.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 
 public class SearchBar {
   public static final Target SEARCH_INPUT = Target.the("Search input field")
-                                                  .located(By.xpath("//input[@type='search']"));
+                                                  .located(By.xpath("//div[@class='component--header__navigation search']//input[@type='search']"));
   public static final Target SEARCH_BUTTON = Target.the("Search button")
                                                    .located(By.xpath("//button[@type='button']"));
 }
